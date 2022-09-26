40bfe71a5e4ba5b63b1ccfa156e1b3e14550d120
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-09-26 17:19:43 +0300

    fix

--- src/js/components/back-to-top.js
+++
@@ -1,7 +0,0 @@
-import { addBackToTop } from 'vanilla-back-to-top';
-
-addBackToTop({
-  diameter: 40,
-  backgroundColor: '#ddd',
-  textColor: '#ff6b08',
-});
--- src/js/components/confetti.js
+++
@@ -1,8 +0,0 @@
-import confetti from 'canvas-confetti';
-
-export default function showConfetti() {
-  confetti.create(document.getElementById('canvas'), {
-    resize: true,
-    useWorker: true,
-  })({ particleCount: 200, spread: 200, zIndex: 2021});
-}
